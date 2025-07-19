7007ad9b9485c4951e8448f7410b505dc517a101
Caleb Cassady <caleb.cassady@hotmail.com>
2025-07-18 22:05:13 -0400

    Enable specifying the rom to run via command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use hachi_emu::Emulator;
 use macroquad::prelude::*;
 
@@ -13,10 +15,19 @@ fn conf() -> Conf {
 
 #[macroquad::main(conf)]
 async fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("Usage: {} <rom-file>", args[0]);
+        return;
+    }
+
+    let rom_name = &args[1];
+
     let mut emulator = Emulator::new();
     emulator.load_font(&hachi_emu::STANDARD_FONT);
 
-    let program = std::fs::read("roms/pong2.rom").unwrap();
+    let program = std::fs::read(rom_name).unwrap();
     emulator.load_program(&program);
 
     emulator.run().await;
